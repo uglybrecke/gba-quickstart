e03fe1faac036aef892ace0ea051e119be6b6254
uglybrecke <206612742+uglybrecke@users.noreply.github.com>
2026-09-22 21:01:09 +0000

    changes speed and color

--- game/src/Main.cpp
+++ game/src/Main.cpp
@@ -8,11 +8,11 @@
 int main() {
     bn::core::init();
 
-    bn::backdrop::set_color(bn::color(0, 0, 0));
+    bn::backdrop::set_color(bn::color(31, 0, 0));
 
     auto dot = bn::sprite_items::dot.create_sprite(0, 0);
 
-    bn::fixed speed = 1.5;
+    bn::fixed speed = 5.5;
 
     while(true) {
         if(bn::keypad::left_held()) {
